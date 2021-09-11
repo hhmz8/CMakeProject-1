483d345457d1b5c006ddf0dc490cac79712c25a8
sleepySva <hhmz8@mail.umsl.edu>
2021-09-11 17:27:02 -0500

    Add project files.

---
+++ CMakeProject-1/CMakeProject-1.cpp
@@ -0,0 +1,13 @@
+﻿// CMakeProject-1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject-1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
+
---
+++ CMakeProject-1/log.h
@@ -0,0 +1,12 @@
+#include <time.h>
+
+typedef struct data_struct {
+	time_t time;
+	char type;
+	char* string;
+} data_t;
+
+int addmsg(const char type, const char * msg);
+void clearlog(void);
+char* getlog(void);
+int savelog(char* filename);
